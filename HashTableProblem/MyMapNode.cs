75e1dbd19e64b36e3fabe06f6da5cd022bb66a27
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-05-13 14:07:06 +0530

    [shravanthi] -Added HashTable using LinkedList

---
+++ HashTableProblem/MyMapNode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableProblem
+{
+    public class MyMapNode<K , V>
+    {
+        public K Key { get; set; }
+        public V Value { get; set; }
+        public MyMapNode<K, V> Next { get; set; }
+        public MyMapNode<K, V> Previous { get; set; }
+
+    }
+}
---
+++ HashTableProblem/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HashTableProblem;
+using System.Collections;
+
+Console.WriteLine("Implementation of Hashtable using LinkedList!");
+HashTableMethods<string, int> hashTable = new(6);
+hashTable.Insert("To", 1);
+hashTable.Insert("be", 2);
+hashTable.Insert("or", 3);
+hashTable.Insert("not", 4);
+hashTable.Insert("to", 5);
+hashTable.Insert("be", 6);
+
+Console.WriteLine("Value of key 'be' is: " + hashTable.GetValue("be"));
