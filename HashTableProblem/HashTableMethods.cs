6efb98b57d562e0a067d24459538fbd1166b7a2c
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-05-14 16:09:25 +0530

    [shravanthi] -Added Uc2 Paragraph Frequency

--- HashTableProblem/HashTableMethods.cs
+++ HashTableProblem/HashTableMethods.cs
@@ -10,39 +10,48 @@ namespace HashTableProblem
     {
         public MyMapNode<K, V>[] Keys;
         public int tableSize;
-
         public HashTableMethods(int maxTableSize)//Constructor
         {
             tableSize = maxTableSize;
             Keys = new MyMapNode<K, V>[tableSize];
-
         }
         public int HashFunction(K key)
         {
             int position = key.GetHashCode() % tableSize; //identifying hash code of key
             return Math.Abs(position);
         }
-
         public void Insert(K key, V value)
         {
             int genIndex = HashFunction(key);
             MyMapNode<K, V> node = Keys[genIndex];
-
             if (node == null)
             {
                 Keys[genIndex] = new MyMapNode<K, V>() { Key = key, Value = value };
                 return;
             }
-
             MyMapNode<K, V> newNode = new() { Key = key, Value = value, Previous = node, Next = null };
             node.Next = newNode;
         }
+        public void Remove(K key, HashTableMethods<K, V> hash)
+        {
+            int genIndex = HashFunction(key);
+            MyMapNode<K, V> node = Keys[genIndex];
+            while(node != null)
+            {
+                if (node.Key.Equals(key) || node.Key.Equals(key))
+                {
 
+                    node.Previous = node.Next;
+                }
+                node = node.Next;
+            }
+            
+        }
         public V GetValue(K key)
         {
             int genIndex = HashFunction(key);
             MyMapNode<K, V> node = Keys[genIndex];
-            while (node != null)//search the linked list to match the key
+            while (node != null)
             {
                 if (node.Key.Equals(key))
                 {
@@ -50,11 +59,8 @@ namespace HashTableProblem
                 }
                 node = node.Next;
             }
-
             throw new Exception("Don't have the key in hash!");
         }
-
-
     }
 }
 
--- HashTableProblem/Program.cs
+++ HashTableProblem/Program.cs
@@ -3,12 +3,47 @@ using HashTableProblem;
 using System.Collections;
 
 Console.WriteLine("Implementation of Hashtable using LinkedList!");
-HashTableMethods<string, int> hashTable = new(6);
-hashTable.Insert("To", 1);
-hashTable.Insert("be", 2);
-hashTable.Insert("or", 3);
-hashTable.Insert("not", 4);
-hashTable.Insert("to", 5);
-hashTable.Insert("be", 6);
 
-Console.WriteLine("Value of key 'be' is: " + hashTable.GetValue("be"));
+while (true)
+{
+    Console.WriteLine("Please choose the option :\n1)uc1\n2)uc2");
+    int option=Convert.ToInt32(Console.ReadLine());
+    switch(option)
+    {
+        case 1:
+            HashTableMethods<string, int> hashTable = new(6);
+            hashTable.Insert("To", 1);
+            hashTable.Insert("be", 2);
+            hashTable.Insert("or", 3);
+            hashTable.Insert("not", 4);
+            hashTable.Insert("to", 5);
+            hashTable.Insert("be", 6);
+            Console.WriteLine("Value of key 'be' is: " + hashTable.GetValue("or"));
+            hashTable.Remove("or", hashTable);
+            Console.WriteLine("Value of key 'be' is: " + hashTable.GetValue("or"));
+            break;
+        case 2:
+            string GivenPara = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            string[] WordsInArray = GivenPara.Split(" ");
+            int tableSize = WordsInArray.Length;
+            HashTableMethods< int, string> hashTable1 = new(tableSize);
+            int Index = 0;
+            foreach (string words in WordsInArray)
+            {
+                hashTable1.Insert(Index, words);
+                Index++;
+            }
+            for (Index = 0; Index < tableSize; Index++)
+            {
+                string word = hashTable1.GetValue(Index);
+                Console.WriteLine(word + "-->" + Index);
+
+            }
+            break;
+        default :
+            Console.WriteLine("Please choose the correct option!");
+            break;
+
+    }
+}
+
